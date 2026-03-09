c8ee4cfcdb196a8593a822c39624f2a1cbb2f494
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:29:53 +0800

    Discovered that isupper() doesn't change letters to uppercase

--- isupper_method.py
+++ isupper_method.py
@@ -1,10 +1,4 @@
 string = "abcdefg hi! eheheheh"
 
 # how isupper() works
-print(string.isupper())
 
-for char in string:
-    if char > string[string.index(char)+1]:
-        print(char)
-
-print(string.index("a"))
