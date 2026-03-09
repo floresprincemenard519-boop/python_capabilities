fbb76e23ea4cacd8a24e735a169a9fa5c0c9c4ca
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 17:58:27 +0800

    made a code and trying to form capitalizze using other methods.

--- capitalize_method.py
+++ capitalize_method.py
@@ -1,4 +1,10 @@
-string = "I should have a capital letter at the beginning."
+string = "i should have a capital letter at the beginning."
 
 # how capitalize() works
 print(string.capitalize())
+
+# how capitalize() works without capitalize() method
+for char in string[0]:
+    if char.islower():
+        char = char.upper()
+        print(char)
