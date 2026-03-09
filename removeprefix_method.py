b09cc16b1488c3d987e6e0178b44d8defc2f4823
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 15:54:33 +0800

    Formed a code that excludes the prefix from printing.

--- removeprefix_method.py
+++ removeprefix_method.py
@@ -1,6 +1,9 @@
 string = "   What a beautiful world! Helloooooo!"
-
+prefix_to_remove = "   W"
 # how removeprefix() works
 print(string.removeprefix('   W'))
 
 # same functionality without using removeprefix()
+# find the index to remove and exclude it fromt the string
+string = string[len(prefix_to_remove):]
+print(string)
