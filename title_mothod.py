4de23c6be1c6238af35524422d4dd235c8cfffe3
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:29:05 +0800

    did some experimenting and still not finding solutions.

--- title_mothod.py
+++ title_mothod.py
@@ -1,14 +1,17 @@
 string = "i am a title of a book"
-
+title_string = ""
 # how title() method works
 # print(string.title())
 
 # how title() method works with title() method
 for char in string:
     if string[string.index(char)-1] == " " or string.index(char) == 0:
-        print(char.upper(), end="")
-    else:
-        print(char, end="")
+        title_string += char.upper()
+    elif string[string.index(char)-1].islower() == True:
+        title_string += char.upper()
+
+
+print(title_string)
 
 # check each letter
 # check if that letter is the beginning of the word
