3c8bc6962ee6e11939f047c849df6ec6d5c10175
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 15:08:10 +0800

    Edited some comments in lstrip.py.

--- lstrip_method.py
+++ lstrip_method.py
@@ -2,6 +2,10 @@ string = "   What a beautiful world! Helloooooo!"
 spaceless_string = ""
 non_space_detected = False
 
+# how lstrip() works
+print(string.lstrip())
+
+# same functionality without using lstrip()
 for char in string:
     if char != " " or non_space_detected:
         spaceless_string += char
