40a95d239a344e81fc1dbede5dfad37c44a68372
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 14:28:05 +0800

    Did the first step.

--- lstrip_method.py
+++ lstrip_method.py
@@ -1,11 +1,15 @@
 # lstrip() example function
 string = "   What a beautiful world! Helloooooo!"
-print(string.lstrip())
-
+# print(string.lstrip())
+spaceless_string = ""
 # remove spaces without using lstrip()
 
 # check if the characters in string is space or not
-
+for char in string:
+    if char != " ":
+        spaceless_string += char
 # if character is space then remove it from the string
 
 # if encountered a non space dont remove spaces further 
+
+print(spaceless_string)
