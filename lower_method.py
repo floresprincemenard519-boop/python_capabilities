a09f3ae9f32f990f000ce2ced5345e53aea39fef
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:57:15 +0800

    Used swapcase for lower.py now i can do it.Now i just have to retain spaces cause it excludes it.

--- lower_method.py
+++ lower_method.py
@@ -5,3 +5,8 @@ print(string.lower())
 
 # how lower() works without lower() method
 # can't do without lower() method cause dont know how convert that to numbers then to acii
+#  but i discovered i can use swapcase so im gonna use that 
+for char in string:
+    if char.isupper():
+        print(char.swapcase(), end='')
+
