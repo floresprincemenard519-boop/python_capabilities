31cb01b667985577e46e731453eff227cbf70220
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:19:40 +0800

    Can't do alternative for lower yet.

--- lower_method.py
+++ lower_method.py
@@ -2,3 +2,6 @@ string = "ABCDEFG HI! EHHEHE"
 
 # how lower() works
 print(string.lower())
+
+# how lower() works without lower() method
+# can't do without lower() method cause dont know how convert that to numbers then to acii
