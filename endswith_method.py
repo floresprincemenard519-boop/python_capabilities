0605fdfce14959e62045031be10eb3e492e79957
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:46:14 +0800

    endswith() without method

--- endswith_method.py
+++ endswith_method.py
@@ -1,4 +1,10 @@
 email_address = "me.@gmail.com"
 
-# how edswith() works
+# how endswith() works
 print(f"Is '{email_address}' a gmail address? {email_address.endswith('@gmail.com')}")
+
+# how endswith() works without endswith() method
+if "@gmail.com" in email_address[-10:]:
+    print(f"Is '{email_address}' a gmail address? True")
+else:
+    print(f"Is '{email_address}' a gmail address? False")
