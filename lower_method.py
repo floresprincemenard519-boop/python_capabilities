38546780e704324c2579732ab5b631ff6515fcc8
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:58:57 +0800

    finished the code just finalizing.

--- lower_method.py
+++ lower_method.py
@@ -1,5 +1,6 @@
 string = "ABCDEFG HI! EHHEHE"
 
+lower_string = ""
 # how lower() works
 print(string.lower())
 
@@ -8,5 +9,7 @@ print(string.lower())
 #  but i discovered i can use swapcase so im gonna use that 
 for char in string:
     if char.isupper():
-        print(char.swapcase(), end='')
+        lower_string += char.swapcase()
+    else:
+        lower_string += char
 
