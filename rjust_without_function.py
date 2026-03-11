d77861dafa846786eb273a06e28e110ee2608bf6
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 03:59:06 +0800

    finished the code and cleaning

--- rjust_without_function.py
+++ rjust_without_function.py
@@ -2,3 +2,10 @@
 # check if length of string is lower than that of the inputed in rjust
 # if so then add spaces or -
 
+string = "I should be 50 characters long, but I'm not."
+rjust_parameter = 50
+
+if len(string) != rjust_parameter:
+    print('-' * (rjust_parameter - len(string)) + string)
+else:
+    print(string)
