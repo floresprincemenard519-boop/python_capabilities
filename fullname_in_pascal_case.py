9b568f1d93b43b30c66ecf645123a794202a1f11
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 01:55:46 +0800

    finished and cleaned the code

--- fullname_in_pascal_case.py
+++ fullname_in_pascal_case.py
@@ -1,18 +1,8 @@
-# get the fullname
-# check each letters and compare if it has space before it if it does it means its a start of a word
-# if a start make capital
-# print all the letters
-
 fullname = input("Please input your fullname: ")
+fullname = fullname.title()
 
-print(fullname.title())
-
-# for index, char in enumerate(fullname):
-#     if index == 0 or fullname[index - 1] == ' ':
-#         print(char.upper(), end="")
-#     elif fullname[index - 1] != ' ':
-#         print(char.lower(), end="")
-#     elif char == ' ':
-#         print('', end='')
-#     else:
-#         print(char, end="")
+for char in fullname:
+    if char == ' ':
+        print('', end='')
+    else:
+        print(char, end="")
