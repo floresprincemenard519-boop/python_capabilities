e91c261e8f659d07a33b326d1c7e61fbb7fe87bc
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 22:48:54 +0800

    Finished the code printed the result using for loop.

--- sorted_list.py
+++ sorted_list.py
@@ -4,3 +4,13 @@
 # if invalid stop the loop
 # sort the list
 # print the list
+list = []
+
+while True:
+    try:
+        numbers = float(input("Please input a number: "))
+        list.append(numbers)
+    except ValueError:
+        for num in sorted(list):
+            print(num, end=" ")
+        break
