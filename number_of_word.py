49451767a118ee565132d45866080becdd8fe0ec
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 01:24:02 +0800

    i used the logic from the title_method.py where the code detects if the last character in the string is a space and if so consider it a word. finalized the code and cleaned it.

--- number_of_word.py
+++ number_of_word.py
@@ -1,4 +1,8 @@
-# get a statement 
-# detect if this is a word
-# if its a word add it to the counter
-# print the amount of words
+word_counter = 0
+statement = input("Please input a full statement: ")
+
+for index, char in enumerate(statement):
+    if index == 0 or statement[index-1] == ' ':
+        word_counter += 1
+        
+print(word_counter)
