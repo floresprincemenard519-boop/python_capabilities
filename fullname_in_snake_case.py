723341f264a95ba107fda5106b4957bed1b9441f
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 02:03:27 +0800

    finished and cleaned the code

--- fullname_in_snake_case.py
+++ fullname_in_snake_case.py
@@ -1,3 +1,8 @@
-# get the full name
-# turn it into lower case
-# replace space with _ underscore
+fullname = input("Please input your fullname in improper casing: ")
+fullname = fullname.lower()
+
+for char in fullname:
+    if char == ' ':
+        print('_', end="")
+    else:
+        print(char, end="")
