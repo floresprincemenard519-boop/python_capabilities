83c88c880cb2a5864e7edf3804fb04f49d7b9814
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 03:47:58 +0800

    tested how startswith works

--- startswith_without_function.py
+++ startswith_without_function.py
@@ -1,3 +1,7 @@
 # get string
 # check if a character is in beginning of the string
 # return False or True
+string = "This string should return True with startswith()."
+beginning = "This"
+
+print(string.startswith("This"))
