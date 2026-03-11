e8bf3c9a3861d9cae68d98f7e915ce8a5599d87f
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:51:24 +0800

    Finalized the code and cleaned of comments.

--- sum_of_multiple_numbers.py
+++ sum_of_multiple_numbers.py
@@ -1,16 +1,12 @@
-# get 1st num
 first_number = float(input("Give me a number: "))
-# get another
 second_number = float(input("Give me another: "))
-# and another
 third_number = float(input("Another: "))
-# sum all of them
-print(first_number + second_number + third_number)
-
 list = []
+
+print(first_number + second_number + third_number)
 
 for number in range(5):
     number = float(input("Give me a number: "))
     list.append(number)
-    
+
 print(sum(list))
