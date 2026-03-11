ee32a82068c80b9ed7c786dd0e5f7702d9669e44
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 04:18:29 +0800

    finished the code and cleaned it used i in for loop.

--- index_without_function.py
+++ index_without_function.py
@@ -1,3 +1,11 @@
 # get a string
 # check each letter for the parameter
 # give the index/range of the parameter
+
+string = "The first location of 'i' is 6."
+index_parameter = 'i'
+
+for i in range(len(string)):
+    if string[i-1] == 'i':
+        print(i)
+        break
