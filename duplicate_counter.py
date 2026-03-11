3ad84a30058e7c039bbe96cab30370721b8289cd
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 04:59:25 +0800

    finished and cleaned the code.

--- duplicate_counter.py
+++ duplicate_counter.py
@@ -3,3 +3,15 @@
 # if is in the list remember them
 # print them 
 
+list = []
+duplicated_numbers = set()
+
+for num in range(10):
+    numbers = float(input("Please input a number: "))
+    if numbers in list:
+        duplicated_numbers.add(numbers)
+    else: 
+        list.append(numbers)
+
+for num in duplicated_numbers:
+    print(num, end=' ')
