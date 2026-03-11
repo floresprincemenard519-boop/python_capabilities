1af3dde2762f244b86ee2c301fb35914c321da16
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 12:54:56 +0800

    Finished the first step in the pseudocode.

--- bigger_number_program.py
+++ bigger_number_program.py
@@ -1,5 +1,6 @@
 # get two numbers
-
+first_number = float(input("Give me a number: "))
+second_number = float(input("Give me another number: "))
 # which of the two numbers is bigger 
 
 # print bigger number 
