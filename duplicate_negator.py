2ade13ea45bdee2b26fd5001994f3b5b0d7e3457
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 22:02:49 +0800

    Finished and cleaned the code,

--- duplicate_negator.py
+++ duplicate_negator.py
@@ -1,9 +1,8 @@
-# get 10 numbers 
-# if number is already inputed dont double it
-# print the numbers
 set = set()
-for num in range(3):
+
+for num in range(10):
     numbers = float(input("give number: "))
     set.add(numbers)
 
-print(set)
+for nums in set:
+    print(nums, end=" ")
