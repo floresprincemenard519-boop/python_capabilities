745fb3e34603de29bd0288321a1b73a3bd6f6f71
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:32:51 +0800

    Followed the psudocode and printed the quotinet.

--- quotient_of_two_numbres.py
+++ quotient_of_two_numbres.py
@@ -1,3 +1,6 @@
 # get the first number
+dividend = float(input("Give me the dividend: "))
 # get the second number
+divisor = float(input("Give me the divisor: "))
 # print the quotient
+print(dividend / divisor)
