3b29acc5b8b2fdad6eaa9199fb1cc0b5e41905bf
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 17:16:30 +0800

    followed the pseudocode and cleaned the code

--- highest_infinite_input.py
+++ highest_infinite_input.py
@@ -2,3 +2,12 @@
 # store the numbers in list
 # get the highest 
 # print the highest 
+list = []
+
+while True:
+    try:
+        numbers = float(input("Please input a number: "))
+        list.append(numbers)
+    except ValueError:
+        print(max(list))
+        break
