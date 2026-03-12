129a5397a484d9b946f3fde637655d52665e6538
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 17:25:51 +0800

    finished the code and cleaned it

--- sorted_infinite_input.py
+++ sorted_infinite_input.py
@@ -3,3 +3,17 @@
 # store number in list
 # sort the list 
 # print the list
+all_numbers = []
+
+while True:
+    try:
+        numbers = float(input("Please input a number: "))
+        all_numbers.append(numbers)
+
+    except ValueError:
+        all_numbers.sort()
+
+        for item in all_numbers:
+            print(item, end=' ')
+
+        break
